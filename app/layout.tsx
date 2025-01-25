7543d849dfcdf01b7d2007073ec02b476d074c06
Varsha Reddy <ushasri.dasari92@gmail.com>
2025-01-25 20:01:14 +0530

    Setup

---
+++ app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+import { Poppins } from 'next/font/google'
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import { urlToHttpOptions } from "url";
+
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  Variable: '--font-poppins',
+});
+
+export const metadata: Metadata = {
+  title: "StoreIt",
+  description: "StoreIt - The Only Storage Solution you need",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${poppins.variable} font-poppins antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
